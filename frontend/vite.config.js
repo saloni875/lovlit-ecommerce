77edf570083bc04b06525c6ded8ca1f9cf854feb
saloni singh <saloni.singh301295@gmail.com>
2026-08-02 10:54:58 +0530

    sitemap and robots added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { sitemap } from "vite-plugin-sitemap";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    sitemap({
-      hostname: "https://lovlit.in",
-    }),
-  ],
+  plugins: [react()],
 
   server: {
     proxy: {
@@ -18,3 +12,25 @@ export default defineConfig({
     },
   },
 });
+
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import { sitemap } from "vite-plugin-sitemap";
+
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     sitemap({
+//       hostname: "https://lovlit.in",
+//     }),
+//   ],
+
+//   server: {
+//     proxy: {
+//       "/api": {
+//         target: "http://localhost:5000",
+//       },
+//     },
+//   },
+// });
